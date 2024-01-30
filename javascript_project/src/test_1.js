26992a0efddf033d562846f88d8e084ed4e3996d
wissentec <142151017+wissentec@users.noreply.github.com>
2024-01-30 10:28:21 +0530

    Add files via upload

---
+++ javascript_project/src/test_1.js
@@ -0,0 +1,10 @@
+function rTriangle(r){
+    for (let i=0;i<r;i++){
+        let r = "";
+        for(let j=1;j<=i;j++){
+            r+="* ";
+        }
+    }
+    console.log(r)
+}
+const noOfRows = 5;
